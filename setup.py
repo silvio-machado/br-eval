b1473301401eb6ac1d593066632470753a9b829e
silvio-machado <machado.vrs@gmail.com>
2024-10-20 21:34:27 -0300

    [SETUP] - Inserting description file to pypi project

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='br-eval',
     version='0.4.2',
     description='Library for validation and formatting of Brazilian data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Silvio Machado',
     author_email='silvio.machado@gmail.com',
     url='https://github.com/silvio-machado/br-eval',
