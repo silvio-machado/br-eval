4914085f2e47e2919db2b511a3cd971015adb3a1
Silvio Machado <machado.vrs@gmail.com>
2024-10-24 10:09:31 -0300

    [SETUP] Updated setup version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='br-eval',
-    version='1.0.0',
+    version='1.0.1',
     description='Library for validation and formatting of Brazilian data',
     long_description=long_description,
     long_description_content_type='text/markdown',
